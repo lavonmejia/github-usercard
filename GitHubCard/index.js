b4255092dda8b5ed8e3ec37496eaa7eb2b63627b
LaVon Mejia <lavonmejia@gmail.com>
2019-07-11 18:35:43 -0400

    array not working

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,10 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+
+// const lavon = axios.get('https://api.github.com/users/lavonmejia')
+// const friendsGits = axios.get(`https://api.github.com/users/${followersArray}`)
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +27,11 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = ['lavonmejia', 'marquesj8023', 'juliethrallstewart', 'dustyfingers'];
+const followersArray = ['dustyfingers' , 'lavonmejia'];
+const friendsGits = axios.get(`https://api.github.com/users/${followersArray}`)
+
+// , "marquesJ8023"
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +52,65 @@ const followersArray = [];
 </div>
 
 */
+
+function createCard(cardObject) {
+  // define new elements
+  console.log(cardObject);
+  cardObject = cardObject.data;
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const cardImg = document.createElement('img');
+  cardImg.src = cardObject.avatar_url;
+  card.appendChild(cardImg);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+
+  const personName = document.createElement('h3');
+  personName.classList.add('name');
+  personName.textContent = cardObject.name;
+  cardInfo.appendChild(personName);
+
+  const personUsername = document.createElement('p');
+  personUsername.classList.add('username');
+  personUsername.textContent = cardObject.login;
+  cardInfo.appendChild(personUsername);
+
+  const personLocation = document.createElement('p');
+  personLocation.innerHTML = `Location: ${cardObject.location}`;
+  cardInfo.appendChild(personLocation);
+
+  const personProfile = document.createElement('p');
+  personProfile.innerHTML = `Profile: <a href=${cardObject.html_url}> ${cardObject.html_url}</a>`;
+  cardInfo.appendChild(personProfile);
+
+  const personFollowers = document.createElement('p');
+  personFollowers.innerHTML = `Followers: ${cardObject.followers}`;
+  cardInfo.appendChild(personFollowers);
+
+  const personFollowing = document.createElement('p');
+  personFollowing.innerHTML = `Following: ${cardObject.following}`;
+  cardInfo.appendChild(personFollowing);
+
+  const personBio = document.createElement('p');
+  personBio.innerHTML = `Bio: ${cardObject.bio}`;
+  cardInfo.appendChild(personBio);
+
+return card
+}
+
+const cards = document.querySelector('.cards');
+// lavon.then(data => {
+//      cards.appendChild(createCard(data))})  
+friendsGits.then(data => {
+        cards.appendChild(createCard(data))
+      console.log('All is well here')});
+
+friendsGits.catch(error => {
+       cards.appendChild(createCard(data))
+      console.log('No worky', error)});
 
 /* List of LS Instructors Github username's: 
   tetondan
